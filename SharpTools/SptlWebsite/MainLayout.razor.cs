04de9b4023a1ad03b0301d880802ab56a841b73c
yueyinqiu <yueyinqiu@outlook.com>
2024-11-29 13:48:02 +0800

    s

--- SharpTools/SptlWebsite/MainLayout.razor.cs
+++ SharpTools/SptlWebsite/MainLayout.razor.cs
@@ -6,11 +6,14 @@ namespace SptlWebsite;
 public partial class MainLayout
 {
     private static readonly ImmutableArray<ToolGroup> Groups = [
+        new ToolGroup("实用工具", "shi2 yong4 gong1 ju4", [
+            new ToolEntry("拼音转换器", "pin1 yin1 zhuan3 huan4 qi4", "/PinyinConverter"),
+            new ToolEntry("计数器", "ji4 shu4 qi4", "/NumberCounter"),
+        ]),
         new ToolGroup("编程工具", "bian1 cheng2 gong1 ju4", [
             new ToolEntry("Guid 生成器", "guid sheng1 cheng2 qi4", "/GuidGenerator"),
             new ToolEntry("字节数组表示", "zi4 jie2 shu4 zu3 biao3 shi4", "/BytesRepresentations"),
             new ToolEntry("Qr 码扫描器", "qr ma3 sao3 miao2 qi4", "/QrCodeScanner"),
-            new ToolEntry("拼音转换器", "pin1 yin1 zhuan3 huan4 qi4", "/PinyinConverter"),
         ]),
         new ToolGroup("易学工具", "yi4 xue2 gong1 ju4", [
             new ToolEntry("六爻预测", "liu4 yao2 yu4 ce4", "/LiuyaoDivination"),
---
+++ SharpTools/SptlWebsite/Pages/NumberCounter/NumberCounterPage.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+using SixLabors.ImageSharp;
+using SptlServices.GradedLocalStoraging;
+using System.Collections.Immutable;
+
+namespace SptlWebsite.Pages.NumberCounter;
+
+public partial class NumberCounterPage
+{
+    private long value = 0;
+    private string ValueDisplay => value is 0 ? "点击计数" : value.ToString();
+
+    private long StepLength
+    {
+        get;
+        set
+        {
+            field = value;
+            this.SavePreference();
+        }
+    } = 1;
+
+    private long valueToSkipTo = 0;
+
+    private sealed record Preferences(long StepLength, long Value);
+
+    private ILocalStorageEntry<Preferences> PreferenceStorage =>
+        this.LocalStorage.GetEntry<Preferences>(
+            "NumberCounterPage.Preferences", Importance.SimpleOptions);
+
+    protected override void OnParametersSet()
+    {
+        if (this.PreferenceStorage.TryGet(out var preference) && preference is not null)
+        {
+            this.StepLength = preference.StepLength;
+            this.valueToSkipTo = preference.Value;
+        }
+    }
+
+    private void SavePreference()
+    {
+        this.PreferenceStorage.Set(new(this.StepLength, this.value));
+    }
+
+    private void Count()
+    {
+        value += StepLength;
+        SavePreference();
+    }
+    private void Skip()
+    {
+        value = valueToSkipTo;
+        SavePreference();
+    }
+}
